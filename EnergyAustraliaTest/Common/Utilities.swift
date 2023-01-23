fd3ec447c890c1a2336e93a6e2cb1e28de7c7aa8
Josevin <jjohny@apple.com>
2023-01-23 12:52:08 +0530

    replacing utility

---
+++ EnergyAustraliaTest/Common/Utilities.swift
@@ -0,0 +1,35 @@
+//
+//  Utilities.swift
+//  EnergyAustraliaTest
+//
+//  Created by Josevin.Johny on 22/01/23.
+//
+
+import Foundation
+import UIKit
+
+extension Data {
+    
+    func convertToJSON() throws -> [[String: Any]]?  {
+        guard let jsonObject = try JSONSerialization.jsonObject(with: self, options: []) as? [AnyObject] else {
+            throw NSError(domain: NSCocoaErrorDomain, code: 1, userInfo: [NSLocalizedDescriptionKey: "Invalid JSON"])
+        }
+        return jsonObject.map { $0 as! [String: Any] }
+    }
+}
+
+extension ViewController {
+    
+    func addAlert(message: String) {
+        let alert = UIAlertController(title: "Try Again", message: message, preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: { (_) in
+            print("User click Dismiss button")
+        }))
+        
+        self.present(alert, animated: true, completion: {
+            print("completion block")
+        })
+    }
+}
+
--- EnergyAustraliaTest/Utilities.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Utilities.swift
-//  EnergyAustraliaTest
-//
-//  Created by Josevin.Johny on 22/01/23.
-//
-
-import Foundation
